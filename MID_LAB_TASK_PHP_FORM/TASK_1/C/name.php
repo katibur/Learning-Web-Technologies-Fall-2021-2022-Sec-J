9809592d10fc6ee1d2d4488252116f1a54326798
katibur <katiburrahmansany@gmail.com>
2021-09-29 23:37:40 +0600

    php form

---
+++ MID_LAB_TASK_PHP_FORM/TASK_1/C/name.php
@@ -0,0 +1,47 @@
+<?php 
+
+	if(isset($_REQUEST['submit']))
+	{
+		$name = $_REQUEST['myname'];
+		if($name == ""){
+			echo "null value...";
+		}else{
+			echo $name;
+		}	
+	}
+	
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Name Field </title>
+</head>
+<body>	
+	<form method="get" action="#">
+		<fieldset>
+			<legend>NAME</legend>
+			
+					<input type="text" name="myname" value="<?php 
+
+	if(isset($_REQUEST['submit']))
+	{
+		$name = $_REQUEST['myname'];
+		if($name == ""){
+			echo "null value...";
+		}else{
+			echo $name;
+		}	
+	}
+
+?>
+"><br/>
+
+<input type="submit" name="submit" value="Submit">
+					
+		</fieldset>
+	</form>
+</body>
+</html>
